e21739a7fa08337c216e46d9710a29909dd0610c
Fabien CHEVREY <fchevrey@student.42.fr>
2019-05-08 19:09:38 +0200

    add min & max to parse_t

--- srcs/parser/centralize_vertex.c
+++ srcs/parser/centralize_vertex.c
@@ -6,14 +6,14 @@
 /*   By: fchevrey <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/04 16:26:34 by fchevrey          #+#    #+#             */
-/*   Updated: 2019/05/04 17:53:08 by fchevrey         ###   ########.fr       */
+/*   Updated: 2019/05/08 19:07:48 by fchevrey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <math.h>
 #include "parser.h"
 
-static t_vecfl		get_center(float *buf, size_t size)
+static t_vecfl		get_center(float *buf, size_t size, t_parse *parse)
 {
 	t_vecfl		min;
 	t_vecfl		max;
@@ -36,6 +36,8 @@ static t_vecfl		get_center(float *buf, size_t size)
 	center.x = (max.x + min.x) * 0.5f;
 	center.y = (max.y + min.y) * 0.5f;
 	center.z = (max.z + min.z) * 0.5f;
+	parse->min =min;
+	parse->max = max;
 	return (center);
 }
 
@@ -46,7 +48,7 @@ void			centralize_vertex(t_float_buf *src)
 
 	if (src->size < 3)
 		return ;
-	center = get_center(src->buf, src->size);
+	center = get_center(src->buf, src->size, parse);
 	i = 0;
 	while (i < src->size)
 	{
